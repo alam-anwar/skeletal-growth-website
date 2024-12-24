478eaf10378b243e07a4356de80e1776b3ba9fb7
alam-anwar <93105876+alam-anwar@users.noreply.github.com>
2024-12-24 14:01:35 -0500

    rolling back changes for GH pages
    better understanding of what the 'base' option does in vite config file

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Title() {
     return (
         <div className="title-bar">
-            <img src="/skeletal-growth-website/deleon-logo.png" />
+            <img src="/deleon-logo.png" />
             <div className="title">
                 <p className="title-skel">Skeletal Growth</p>
                 <p className="title-deleon">DeLeon Lab at the UF Anthropology Department</p>
