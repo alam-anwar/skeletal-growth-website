390ec0491f96e2560414d3876879a1ab16ebd439
alam-anwar <93105876+alam-anwar@users.noreply.github.com>
2025-04-07 10:41:29 -0400

    modified navbar and react routes to reflect new page structure

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,20 @@ import Index from './components/Index'
 import About from './components/About'
 import Navbar from './components/Navbar'
 import Template from './components/Template'
+import CranialBase from './pages/CranialBase'
+import DeleonLabResources from './pages/DeLeonLabResources'
+import GalagoLimbs from './pages/GalagoLimbs'
+import Haplorhines from './pages/Haplorhines'
+import LearningModules from './pages/LearningModules'
+import MorphoSourceProjects from './pages/MorphoSourceProjects'
+import NasalCavity from './pages/NasalCavity'
+import People from './pages/People'
+import PostorbitalSeptation from './pages/PostorbitalSeptation'
+import Strepsirrhines from './pages/Strepsirrhines'
+import ToothJaw from './pages/ToothJaw'
+import Projects from './pages/Projects'
+import Taxonomy from './pages/Taxonomy'
+import Resources from './pages/Resources'
 
 function App() {
     return (
@@ -20,8 +34,22 @@ function App() {
                 <Title />
                 <Navbar />
                 <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/phylo-tree' element={<PhyloTreeOrtho />} />
+                    <Route path='/home' element={<Home />} />
+                    <Route path='/projects' element={<Projects />} />
+                    <Route path='/projects/tooth-jaw' element={<ToothJaw />} />
+                    <Route path='/projects/cranial-base' element={<CranialBase />} />
+                    <Route path='/projects/nasal-cavity' element={<NasalCavity />} />
+                    <Route path='/projects/postorbital-septation' element={<PostorbitalSeptation />} />
+                    <Route path='/projects/galago-limbs' element={<GalagoLimbs />} />
+                    <Route path='/taxonomy' element={<Taxonomy />} />
+                    <Route path='/taxonomy/haplorhines' element={<Haplorhines />} />
+                    <Route path='/taxonomy/strepsirrhines' element={<Strepsirrhines />} />
+                    <Route path='/taxonomy/phylogenetic-tree' element={<PhyloTreeOrtho />} />
+                    <Route path='/people' element={<People />} />
+                    <Route path='/resources' element={<Resources />} />
+                    <Route path='/resources/learning-modules' element={<LearningModules />} />
+                    <Route path='/resources/morpho-source-projects' element={<MorphoSourceProjects />} />
+                    <Route path='/resources/deleon-lab-resources' element={<DeleonLabResources />} />
                     <Route path='/index' element={<Index />} />
                     <Route path='/about' element={<About />} />
                     <Route path='/template' element={<Template />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,13 +6,34 @@ export default function Navbar() {
     return (
         <>
             <div className="navbar">
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/phylo-tree">DEBUG: Phylogenetic Tree</Link></li>
-                    <li><Link to="/index">Index</Link></li>
-                    <li><Link to="/template">Template</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                </ul>
+                <Link to="/home">Home</Link>
+                <div className="dropdown">
+                    <button className="dropbtn">Projects</button>
+                    <div className="dropdown-content">
+                        <Link to="/projects/tooth-jaw">Tooth Jaw</Link>
+                        <Link to="/projects/cranial-base">Cranial Base</Link>
+                        <Link to="/projects/nasal-cavity">Nasal Cavity</Link>
+                        <Link to="/projects/postorbital-septation">Postorbital Septation</Link>
+                        <Link to="/projects/galago-limbs">Galago Limbs</Link>
+                    </div>
+                </div>
+                <div className="dropdown">
+                    <button className="dropbtn">Taxonomy</button>
+                    <div className="dropdown-content">
+                        <Link to="/taxonomy/haplorhines">Haplorhines</Link>
+                        <Link to="/taxonomy/strepsirrhines">Strepsirrhines</Link>
+                        <Link to="/taxonomy/phylogenetic-tree">Phylogenetic Tree</Link>
+                    </div>
+                </div>
+                <Link to="/people">People</Link>
+                <div className="dropdown">
+                    <button className="dropbtn">Resources</button>
+                    <div className="dropdown-content">
+                        <Link to="/resources/learning-modules">Learning Modules</Link>
+                        <Link to="/resources/morpho-source-projects">MorphoSource Projects</Link>
+                        <Link to="/resources/deleon-lab-resources">DeleonLab Resources</Link>
+                    </div>
+                </div>
             </div>
         </>
     )
