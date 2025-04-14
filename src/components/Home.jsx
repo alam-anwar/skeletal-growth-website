305b4645d32b75f0959115c6f95b21f857ae0a86
alam-anwar <93105876+alam-anwar@users.noreply.github.com>
2025-04-14 10:40:40 -0400

    added and tested old unity viewer on webpage
    ignoring it on git because of size, might add it as a submodule

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { Unity, useUnityContext } from 'react-unity-webgl'
 
 export default function Home() {
+    const { unityProvider } = useUnityContext({
+        loaderUrl: "./uv-build/Build/uv-build.loader.js",
+        dataUrl: "./uv-build/Build/uv-build.data.br",
+        frameworkUrl: "./uv-build/Build/uv-build.framework.js.br",
+        codeUrl: "./uv-build/Build/uv-build.wasm.br"
+    })
+
     return (
-        <div>
-            <h1>Welcome!</h1>
-            <p>This is the website for the Skeletal Growth team.</p>
-            <p>Stay tuned as we update this website with more information!</p>
-        </div>
+        <>
+            <div>
+                <h1>Welcome!</h1>
+                <p>This is the website for the Skeletal Growth team.</p>
+                <p>Stay tuned as we update this website with more information!</p>
+            </div>
+            <div className='unity-viewer'>
+                <Unity unityProvider={unityProvider} style={{width: 800, height: 450}} />
+            </div>
+        </>
     )
 }
