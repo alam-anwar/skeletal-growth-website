04a62c991c3d6ef3d2c8573018d25d3ed5fb9096
Al Anwar <alamanwar17@gmail.com>
2025-09-15 14:30:50 -0400

    Refactored and restylized the navbar
    For the purpose of directing auditors towards pages that are fleshed out, several pages have been removed from the navbar. Additionally, the navbar has been restylized to look better (for lack of a better word).

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { slide as Menu } from 'react-burger-menu'
 
-export default function Navbar() {
+export default function NavDesktop() {
     return (
         <>
             <div className="navbar">
+                <img src="deleon-logo.png" />
+                <div className="site-title">
+                    <h1>Skeletal Growth</h1>
+                    <p style={{fontStyle: 'italic'}}>Developmental Mechanisms and Evolutionary History</p>
+                </div>
                 <Link to="/">Home</Link>
                 <div className="dropdown">
                     <button className="dropbtn">Projects</button>
                     <div className="dropdown-content">
-                        <Link to="/projects/tooth-jaw">Tooth Jaw</Link>
-                        <Link to="/projects/cranial-base">Cranial Base</Link>
+                        <Link to="/projects/tooth-jaw">Tooth and Jaw</Link> {/* KEEP */}
+                        {/* <Link to="/projects/cranial-base">Cranial Base</Link>
                         <Link to="/projects/nasal-cavity">Nasal Cavity</Link>
                         <Link to="/projects/postorbital-septation">Postorbital Septation</Link>
-                        <Link to="/projects/galago-limbs">Galago Limbs</Link>
+                        <Link to="/projects/galago-limbs">Galago Limbs</Link> */}
                     </div>
                 </div>
                 <div className="dropdown">
@@ -25,15 +29,19 @@ export default function Navbar() {
                         <Link to="/taxonomy/phylogenetic-tree">Phylogenetic Tree</Link>
                     </div>
                 </div>
-                <Link to="/people">People</Link>
-                <div className="dropdown">
+                <div className="dropdown dropdown-right">
                     <button className="dropbtn">Resources</button>
                     <div className="dropdown-content">
-                        <Link to="/resources/learning-modules">Learning Modules</Link>
+                        {/* <Link to="/resources/learning-modules">Learning Modules</Link> */}
                         <Link to="/resources/morpho-source-projects">MorphoSource Projects</Link>
                         <Link to="/resources/deleon-lab-resources">DeleonLab Resources</Link>
+                        <Link to="/resources/publications">Pages and Publications</Link>
                     </div>
                 </div>
+                <Link to="/about">About Us</Link>
+            </div>
+            <div class>
+
             </div>
         </>
     )
