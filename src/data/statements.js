64170535bf36469c0a33656181fbee09903c3e18
Al Anwar <alamanwar17@gmail.com>
2025-10-23 22:08:34 -0400

    Removed statements.js
    No need for it, since DB loading is handled in another file.

--- src/data/statements.js
+++
@@ -1,9 +0,0 @@
-// import { initSqlJs } from "sql.js";
-
-// const sqlPromise = initSqlJs({
-//     locateFile: '/assets/sql-wasm.wasm'
-// })
-
-// const dataPromise = fetch("/assets/databases/skeletal-growth.db").then(res => res.arrayBuffer());
-// const [SQL, buf] = await Promise.all([sqlPromise, dataPromise]);
-// export const db = new SQL.database(new Uint8Array(buf));
