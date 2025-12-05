3ddb2ab66e2db07104c22566a4cf46b62b0108b5
Al Anwar <alamanwar17@gmail.com>
2025-12-05 14:09:40 -0500

    Fixed input error on debug page

--- src/pages/TestDebug.jsx
+++ src/pages/TestDebug.jsx
@@ -3,32 +3,33 @@ import { Unity, useUnityContext } from 'react-unity-webgl'
 import { useState } from 'react'
 
 export default function TestDebug() {
-    const [url, setUrl] = useState("")
+  const [url, setUrl] = useState("")
 
-    const { unityProvider, sendMessage } = useUnityContext({
-        loaderUrl: "./OsteologyViewerBuild/Build/OsteologyViewerBuild.loader.js",
-        dataUrl: "./OsteologyViewerBuild/Build/OsteologyViewerBuild.data",
-        frameworkUrl: "./OsteologyViewerBuild/Build/OsteologyViewerBuild.framework.js",
-        codeUrl: "./OsteologyViewerBuild/Build/OsteologyViewerBuild.wasm"
-    })
+  const { unityProvider, sendMessage } = useUnityContext({
+    loaderUrl: "./OsteologyViewerBuild/Build/OsteologyViewerBuild.loader.js",
+    dataUrl: "./OsteologyViewerBuild/Build/OsteologyViewerBuild.data",
+    frameworkUrl: "./OsteologyViewerBuild/Build/OsteologyViewerBuild.framework.js",
+    codeUrl: "./OsteologyViewerBuild/Build/OsteologyViewerBuild.wasm"
+  })
 
-    function sendUrlToUnity() {
-        sendMessage("Manager", "LoadFromHTML", url);
-    }
+  function sendUrlToUnity() {
+    sendMessage("Manager", "LoadFromHTML", url);
+  }
 
-    function handleChange(e) {
-        setUrl(e.target.value);
-    }
-    
-    return (
-        <>
-            <Unity unityProvider={unityProvider} style={{width: 800, height: 450}}/>
-            <form>
-                <label>Enter URL:
-                    <input type="text" value={url} onChange={handleChange} />
-                </label>
-                <input type="button" onClick={sendUrlToUnity} value="Load Model"/>
-            </form>
-        </>
-    )
+  const handleInputChange = (event) => {
+    setUrl(event.target.value);
+  }
+
+  return (
+    <>
+      <Unity unityProvider={unityProvider} style={{ width: 800, height: 450, pointerEvents: 'none' }} />
+
+      <div style={{ marginTop: '30px' }}>
+        <label>
+          Enter URL: <input type="text" value={url} onChange={handleInputChange} />
+        </label>
+        <input type="button" onClick={sendUrlToUnity} value="Load Model" />
+      </div>
+    </>
+  )
 }
